c039c997a58a97423491dcbe45caff25fe12574d
Jaime, David <davjaime1@yahoo.com>
2019-11-07 13:43:20 -0600

    Selenium Register Function Added
    - The Register Function is created
    - The sharedUIMap was updated to work with the register page
    - Test werent made for either Login or Register, I think thats whoever is doing the User validations
    - The functions however are used during the rest of the System Users scenarios

--- test/mac_repair/selenium/SeleniumTC01.java
+++ test/mac_repair/selenium/SeleniumTC01.java
@@ -1,15 +1,8 @@
 package mac_repair.selenium;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -19,14 +12,13 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
-import mac_repair.MRFunctions;
-import mac_repair.model.User;
-import mac_repair.util.SQLConnection;
 import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
+import mac_repair.MRFunctions;
+import mac_repair.model.User;
+import mac_repair.model.UserTest;
+import mac_repair.util.SQLConnection;
 
 @RunWith(JUnitParamsRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) // the test methods in this test file can run in any order but I prefer a fixed order
@@ -34,6 +26,8 @@ public class SeleniumTC01 extends MRFunctions {
   static SQLConnection DBMgr = SQLConnection.getInstance();
   private StringBuffer verificationErrors = new StringBuffer();
   public String sAppURL, sSharedUIMapPath, testDelay;
+  User u;
+  UserTest error;
   
   @Before
   public void setUp() throws Exception {
@@ -46,14 +40,27 @@ public class SeleniumTC01 extends MRFunctions {
 	sSharedUIMapPath = prop.getProperty("SharedUIMap");
 	testDelay=prop.getProperty("testDelay");
 	prop.load(new FileInputStream(sSharedUIMapPath));
+	u = new User();
+	error = new UserTest();
   }
     
   @Test
   @FileParameters("test/mac_repair/selenium/TC01_test_cases.csv")
-  public void TC01(int testCaseNumber, String username, String password) throws Exception
+  public void TC01(int testCaseNumber, String username, String password, String errorMessage ) throws Exception
   {
 	  driver.get(sAppURL);
 	  MR_Login(driver, username, password);
+	  //Need to pull login error message from get method, but it doesn't exist anymore after the clean
+	  //assertTrue(errorMessage.equals())
+  }
+  
+  @Test
+  @FileParameters("test/mac_repair/selenium/TC01b_test_cases.csv")
+  public void TC01b(int testCaseNumber, String username, String utaID, String first, String last, String password, 
+					String address, String city, String zip, String state, String phone, String email, String role) throws Exception
+  {
+	  driver.get(sAppURL);
+	  MR_Register(driver, username, utaID, first, last, password, address, city, zip, state, phone, email, role);
   }
 
   @After
