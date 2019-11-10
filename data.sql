61a56c72232d429af0c401cb432894a33869de86
Jaime, David <davjaime1@yahoo.com>
2019-11-10 11:29:00 -0600

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,3 @@
-
 USE `database`;
 INSERT INTO users VALUE ("user1","1001526202","SM","SM","password","U","1121 UTA","Arlington","TX","76010","1001526202","sm@uta.edu");
 INSERT INTO users VALUE ("usr1","1001526202","SM","SM","password","U","1121 UTA","Arlington","TX","76010","1001526202","sm@uta.edu");
@@ -10,42 +9,51 @@ INSERT INTO users VALUE ("rep","1001526202","SM","SM","password","R","1121 UTA",
 INSERT INTO users VALUE ("rep5","1001526202","SM","SM","password","R","1121 UTA","Arlington","TX","76010","1001526202","sm@uta.edu");
 
 
-
-INSERT INTO mar VALUE ("1",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
-INSERT INTO mar VALUE ("2",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
-INSERT INTO mar VALUE ("3",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
-INSERT INTO mar VALUE ("4",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
-INSERT INTO mar VALUE ("5",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
-
-INSERT INTO mar VALUE ("6",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",curdate(),"1D"); 
-INSERT INTO mar VALUE ("7",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",curdate(),"1D"); 
-INSERT INTO mar VALUE ("8",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",curdate(),"1D"); 
-INSERT INTO mar VALUE ("9",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("10",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("11",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("12",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 2 DAY,"1D"); 
-INSERT INTO mar VALUE ("13",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 2 DAY,"1D"); 
-INSERT INTO mar VALUE ("14",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 2 DAY,"1D"); 
-INSERT INTO mar VALUE ("15",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 2 DAY,"1D"); 
+INSERT INTO mar VALUE ("71",curdate(),"BMC","BMC1","des","Ma","usr1",null ,null ,null); 
+INSERT INTO mar VALUE ("72",curdate(),"BMC","BMC3","des","Ma","usr1",null,null,null); 
+INSERT INTO mar VALUE ("73",curdate(),"BMC","BMC4","des","Ma","usr1",null,null,null); 
+INSERT INTO mar VALUE ("74",curdate(),"BMC","BMC4","des","Ma","usr1",null,null,null); 
+INSERT INTO mar VALUE ("75",curdate(),"BMC","BMC4","des","Ma","usr1",null,null,null); 
 
 
-INSERT INTO mar VALUE ("21",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("22",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("23",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("24",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("25",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D");
+INSERT INTO mar VALUE ("101",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
+INSERT INTO mar VALUE ("102",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
+INSERT INTO mar VALUE ("103",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
+INSERT INTO mar VALUE ("104",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
+INSERT INTO mar VALUE ("105",curdate(),"BMC","BMC1","des","Ma","usr1","rep2",curdate(),"1D"); 
 
-INSERT INTO mar VALUE ("31",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",curdate(),"1D"); 
-INSERT INTO mar VALUE ("32",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",curdate(),"1D"); 
-INSERT INTO mar VALUE ("33",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",curdate(),"1D"); 
-INSERT INTO mar VALUE ("34",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE(),"1D"); 
-INSERT INTO mar VALUE ("35",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE(),"1D"); 
-INSERT INTO mar VALUE ("36",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("37",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("38",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 1 DAY,"1D"); 
-INSERT INTO mar VALUE ("39",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 2 DAY,"1D"); 
-INSERT INTO mar VALUE ("40",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 2 DAY,"1D");
+INSERT INTO mar VALUE ("106",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",curdate(),"1D"); 
+INSERT INTO mar VALUE ("107",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",curdate(),"1D"); 
+INSERT INTO mar VALUE ("108",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",curdate(),"1D"); 
+INSERT INTO mar VALUE ("109",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("110",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("111",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("112",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 2 DAY,"1D"); 
+INSERT INTO mar VALUE ("113",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 2 DAY,"1D"); 
+INSERT INTO mar VALUE ("114",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 2 DAY,"1D"); 
+INSERT INTO mar VALUE ("115",curdate(),"BMC","BMC1","des","Ma","usr1","rep3",CURDATE() + INTERVAL 2 DAY,"1D"); 
 
+
+INSERT INTO mar VALUE ("121",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("122",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("123",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("124",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("125",CURDATE() - INTERVAL 1 DAY,"BMC","BMC1","des","Ma","usr1","rep4",CURDATE() - INTERVAL 1 DAY,"1D");
+
+INSERT INTO mar VALUE ("131",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",curdate(),"1D"); 
+INSERT INTO mar VALUE ("132",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",curdate(),"1D"); 
+INSERT INTO mar VALUE ("133",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",curdate(),"1D"); 
+INSERT INTO mar VALUE ("134",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE(),"1D"); 
+INSERT INTO mar VALUE ("135",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE(),"1D"); 
+INSERT INTO mar VALUE ("136",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("137",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("138",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 1 DAY,"1D"); 
+INSERT INTO mar VALUE ("139",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 2 DAY,"1D"); 
+INSERT INTO mar VALUE ("140",curdate(),"BMC","BMC1","des","Ma","usr1","rep5",CURDATE() + INTERVAL 2 DAY,"1D"); 
+
+
+INSERT INTO facilityreservation VALUE("27", "BMC1", "BMC", "I", "rep", "2019-10-25", "2019-10-25 06:29:59", "2019-10-25 06:00:00");
+INSERT INTO facilityreservation VALUE("30", "BMC1", "BMC", "I", "rep", "2019-10-25", "2019-10-25 12:59:59", "2019-10-25 12:30:00");
 
 INSERT INTO users VALUE ("repTest","1001526202","SM","SM","password","R","1121 UTA","Arlington","TX","76010","1001526202","sm@uta.edu");
 INSERT INTO mar VALUE ("50","2019-10-24","BMC","BMC1","des","Ma","usr1","repTest","2019-10-25","1D");
